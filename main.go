1f6181dfb255feed640f73bb95163537aa8268c8
Roneet Kumar <roneetparida@gmail.com>
2020-05-22 02:59:21 -0400

    client

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"net"
+)
+
+func main() {
+
+	s := newServer()
+
+	listener, err := net.Listen("tcp", ":8888")
+
+	if err != nil {
+		log.Fatalf("unable to start server: %s", err.Error())
+	}
+	defer listener.Close()
+	log.Printf("Started server on :8888")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("unable to accept connection: %s", err.Error())
+			continue
+		}
+
+		go s.newClient(conn)
+
+	}
+
+}
