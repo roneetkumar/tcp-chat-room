152e26637f9f45bf595475bf600112d1b2263e77
Roneet Kumar <roneetparida@gmail.com>
2020-05-22 02:58:40 -0400

    server setup

---
+++ server.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"net"
+)
+
+type server struct {
+	rooms    map[string]*room
+	commands chan command
+}
+
+func newServer() *server {
+	return &server{
+		rooms:    make(map[string]*room),
+		commands: make(chan command),
+	}
+}
+
+func (s *server) newClient(conn net.Conn) {
+	log.Printf("new client has connected: %s", conn.RemoteAddr().String())
+
+	c := &client{
+		conn:     conn,
+		nick:     "anonymous",
+		commands: s.commands,
+	}
+
+	c.readInput()
+
+}
